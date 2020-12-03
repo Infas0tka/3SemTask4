497e7de2729124862eb32273e05c7727dfcd8b07
Infas0tka <65242283+Infas0tka@users.noreply.github.com>
2020-12-03 18:14:23 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    char stroka[200];
+    ifstream fin("input.txt");
+    fin.getline(stroka, 200);
+    fin.close();
+    string s;
+    s = string (stroka);
+    ofstream fout("output.txt");
+    fout << s << endl;
+    fout << endl;
+    string::iterator it;
+
+
+
+
+
+    while(s.find("  ") != string::npos)       //No spaces
+        s.erase(s.find("  "), 1);
+    if (*(s.begin()) == ' ')
+        s.erase(s.begin());
+    if (*(s.end() - 1) == ' ')
+        s.erase(s.end() - 1);
+    if ((*(s.end() - 1) == '.') && (*(s.end() - 2) == ' '))
+    {
+        s.erase(s.end() - 1);
+        s.erase(s.end() - 1);
+    }
+    if ((*(s.end() - 1) == '.') && (*(s.end() - 2) != ' '))
+        s.erase(s.end() - 1);
+    fout << "No extra spaces:               " << s << endl;
+
+
+
+
+
+    int j = s.rfind(" ");                   //Find and delete all the same words as last
+    string last = "";
+    it = s.end() - 1;
+    while (*it != ' ')
+    {
+        last = *it + last;
+        it--;
+    }
+    int dlina = last.length();
+    j = -1;
+    while(s.find(last, j + 1) != string::npos)
+    {
+        j = s.find(last, j + 1);
+        if ((j > 0) && (s[j - 1] == ' '))
+        {
+            if ((j < dlina + s.length()) && (s[j + dlina] == ' '))
+                s.erase(j, dlina);
+        }
+        else if (j == 0)
+        {
+            if ((j < dlina + s.length()) && (s[j + dlina] == ' '))
+                s.erase(j, dlina);
+        }
+    }
+    it = s.begin();
+    s.erase(s.length() - last.length(), last.length());
+    s.erase(s.end() - 1);
+    while(s.find("  ") != string::npos)       //No spaces
+        s.erase(s.find("  "), 1);
+    if (*(s.begin()) == ' ')
+        s.erase(s.begin());
+    if (*(s.end() - 1) == ' ')
+        s.erase(s.end() - 1);
+    fout << "Deleted all 'last' words:      " << s << endl;
+
+
+
+
+
+    it = s.begin();               //Delete last letter
+    while (it != s.end())
+    {
+        if (*it == ' ')
+            s.erase(it - 1);
+        else
+        it++;
+    }
+    if (it == s.end())
+        s.erase(it - 1);
+    fout << "Deleted last letter:           " << s << endl;
+    fout.close();
+    return 0;
+}
